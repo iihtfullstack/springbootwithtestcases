54ab5fe3028c6519acc6b63cd474cc15ec938c34
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2019-03-26 00:40:36 -0400

    changing again

--- src/main/java/com/sunil/capsule/app/TaskManagerApp.java
+++ src/main/java/com/sunil/capsule/app/TaskManagerApp.java
@@ -1,21 +1,21 @@
-package com.sunil.springData.app;
+package com.sunil.capsule.app;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "com.sunil.springData")
-@EntityScan(basePackages = "com.sunil.springData.model")
-@EnableJpaRepositories("com.sunil.springData.repository")
+@ComponentScan(basePackages = "com.sunil.capsule")
+@EntityScan(basePackages = "com.sunil.capsule.model")
+//@EnableJpaRepositories("com.sunil.capsule.repository")
 
-public class Application extends WebMvcConfigurerAdapter {
+public class TaskManagerApp extends WebMvcConfigurerAdapter {
 
 	  public static void main(String[] args) throws Exception {
-	        SpringApplication.run(Application.class, args);
+	        SpringApplication.run(TaskManagerApp.class, args);
 	    }
 }
--- src/main/java/com/sunil/springData/menu/UserMenu.java
+++
@@ -1,8 +0,0 @@
-package com.sunil.springData.menu;
-
-
-public enum UserMenu {
-	ADD_SUBJECT, DELETE_SUBJECT, SEARCH_SUBJECT,
-	ADD_BOOK, DELETE_BOOK, SEARCH_BOOK,
-	EXIT;
-}
